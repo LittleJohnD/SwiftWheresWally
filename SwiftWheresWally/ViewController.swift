c545943db227ccca90e83c5f997e133e9392de9e
John Dyer <jbdyer@gmail.com>
2015-01-20 11:29:36 +0000

    Step 1 complete

--- SwiftWheresWally/ViewController.swift
+++ SwiftWheresWally/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameEntry: UITextField!
+    @IBOutlet weak var shownName: UILabel!
+    @IBOutlet weak var lastButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +22,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func showNext(sender: UIButton) {
+    }
+    
+    @IBAction func showLast(sender: UIButton) {
+    }
+    @IBAction func addName(sender: UIButton) {
+        
     }
 
 
